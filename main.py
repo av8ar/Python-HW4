391545465ee257815296eb7d9dd3b0a8faccde03
Victor Dai <69231171+av8ar@users.noreply.github.com>
2021-06-25 16:20:07 +0000

    Modified main method

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ class IPAddressConverter:
             address = str(num % 256) + '.' + address
             num /= 256
         return address
+
     #converts ipAddress to its equivalent number form
     def ipAddressToNum(address):
         num = 0
         exp = 3
         intList = address.split('.')
         for i in intList:
-            num += i * (256**exp)
+            num += i * (256 ** exp)
             exp -= 1
         return num
 
@@ -28,9 +29,11 @@ class MonoAlphabeticCipher:
     digits = '0123456789'
     char_str = upper_alphabet + lower_alphabet + digits
 
+    #Instantiation
     def __init__(self):
         self.key = self.generateKey()
 
+    #Generates key
     def generateKey(self):
         global char_str
         key = ''
@@ -40,7 +43,8 @@ class MonoAlphabeticCipher:
                 key.append(c)
         self.key = key
         return key
-
+    
+    #Encrypts message
     def encrypt(self, msg):
         global char_str
         encrypted_msg = ''
@@ -48,6 +52,7 @@ class MonoAlphabeticCipher:
             encrypted_msg.append(self.key[char_str.index(c)])
         return encrypted_msg
 
+    #Decrypts encrypted message
     def decrypt(self, encrypted_msg):
         global char_str
         decrypted_msg = ''
@@ -55,15 +60,20 @@ class MonoAlphabeticCipher:
             decrypted_msg.append(char_str[self.key.index(c)])
         return decrypted_msg
 
-
+#Main function
 def main():
     print('Hello World')
     converter = IPAddressConverter()
     address1 = '127.0.0.1'
     num1 = 2130706433
+    address2 = '0.0.0.0'
+    num2 = 0
+
     print(address1, '=', converter.ipAddressToNum(address1))
     print(num1, '=', converter.numToIpAddress(num1))
+    print(address2, '=', converter.numToIpAddress(address2))
+    print(num2, '=', converter.numToIpAddress(num2))
 
-
+#Executes main method
 if __name__ == '__main__':
     main()
