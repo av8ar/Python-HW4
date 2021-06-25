7933a12981e1432f6de4acb036cf00aec6bb6ec7
Victor Dai <69231171+av8ar@users.noreply.github.com>
2021-06-25 04:42:21 +0000

    Changes to main call and more

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-#Does the conversions
 import random
-#import debugpy
 
-
+#Does the conversions
 class IPAddressConverter:
+    #converts number to its equivalent IP Address
     def numToIpAddress(num):
         address = ''
         while num > 0:
             address = str(num % 256) + '.' + address
             num /= 256
         return address
-
+    #converts ipAddress to its equivalent number form
     def ipAddressToNum(address):
         num = 0
         exp = 3
@@ -20,7 +19,7 @@ class IPAddressConverter:
             exp -= 1
         return num
 
-
+#Generates key, encrypts, decrypts messages
 class MonoAlphabeticCipher:
 
     upper_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -32,24 +31,27 @@ class MonoAlphabeticCipher:
         self.key = self.generateKey()
 
     def generateKey(self):
+        global char_str
         key = ''
         for i in range(62):
-            c = self.char_str[random.randint(0, 61)]
+            c = char_str[random.randint(0, 61)]
             if c not in key:
-                key += c
+                key.append(c)
         self.key = key
         return key
 
     def encrypt(self, msg):
+        global char_str
         encrypted_msg = ''
         for c in msg:
-            encrypted_msg += self.key[self.char_str.index(c)]
+            encrypted_msg.append(self.key[char_str.index(c)])
         return encrypted_msg
 
     def decrypt(self, encrypted_msg):
+        global char_str
         decrypted_msg = ''
         for c in encrypted_msg:
-            decrypted_msg += self.char_str[self.key.index(c)]
+            decrypted_msg.append(char_str[self.key.index(c)])
         return decrypted_msg
 
 
@@ -60,3 +62,7 @@ def main():
     num1 = 2130706433
     print(address1, '=', converter.ipAddressToNum(address1))
     print(num1, '=', converter.numToIpAddress(num1))
+
+
+if __name__ == "main":
+    main()
