f5d4d75a0a324d710ed56edffd14aa34a48c0df8
Victor Dai <69231171+av8ar@users.noreply.github.com>
2021-06-27 17:56:36 +0000

    modified

--- main.py
+++ main.py
@@ -33,8 +33,7 @@ class IPAddressConverter:
 
 class IPAddressConverterTest(unittest.TestCase):
     
-    def test_IPAddressConverterTest(self):
-          
+    def test_IPAddressConverterTest(self):   
         addressList = ['127.0.0.1','0.0.0.0','101.0.78.13','9.9.9.10','10.6.8.130','255.255.255.255','6.6.6.6','220.36.180.140','0.0.250.4','9.207.201.0'] 
         numList = [2130706433,0,1694518797,151587082,168167554,4294967295,101058054,3693393036,64004,164612352]  
         print('IPAddressConverterTests for ipAddressToNum and numToIpAddress:\n') 
@@ -42,16 +41,16 @@ class IPAddressConverterTest(unittest.TestCase):
             self.assertEqual(IPAddressConverter.ipAddressToNum(addressList[i]), numList[i])
             print('\nipAddressToNumTest' + str(i+1), '\n' + addressList[i], '=', numList[i])
             self.assertEqual(IPAddressConverter.numToIpAddress(numList[i]),addressList[i])
-            print('\nnumToIpAddressTest' + str(i+1), '\n' + numList[i], '=', addressList[i]    
+            print('\nnumToIpAddressTest' + str(i+1), '\n' + numList[i], '=', addressList[i])    
 
 #Generates key, encrypts, decrypts messages
 class MonoalphabeticCipher:
-
+    
     lower_alphabet = 'abcdefghijklmnopqrstuvwxyz'
     upper_alphabet = lower_alphabet.upper()
     digits = '0123456789'
     char_str = upper_alphabet + lower_alphabet + digits
-
+    
     #Instantiation
     def __init__(self):
         self.key = self.generateKey()
